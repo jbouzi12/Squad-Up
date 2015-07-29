c3abb2151492cad5b1e29326153a6457889bdeb7
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-07-29 13:33:58 -0400

    add legend colors and keys

--- js/app.js
+++ js/app.js
@@ -1,2 +1,37 @@
 (function() {
+
+	var colors = d3.scale.quantize()
+		.range(colorbrewer.Blues[7]);
+
+	// var legend = d3.select('#legendbox')
+	// 	.append('ul')
+	// 	.attr('class', 'list-inline');
+
+	// var keys = legend.selectAll('li.key')
+	// 	.data(colors.range());
+
+	// keys.enter().append('li')
+	// 	.attr('class', 'key')
+	// 	.style('border-top-color', String)
+	// 	.text(function(d) {
+	// 		var r = colors.invertExtent(d);
+	// 		return formats.percent(r[0])
+	// 	});
+var legend = d3.select('#legendbox')
+  .append('ul')
+    .attr('class', 'list-inline');
+
+var keys = legend.selectAll('li.key')
+    .data(colors.range());
+
+keys.enter().append('li')
+    .attr('class', 'key')
+    .style('border-top-color', String)
+    .text(function(d) {
+    	// Convert hexadecimal color to percentage range for that color
+        var r = colors.invertExtent(d);
+        // Format to percent 
+        var formats = d3.format('%')
+        return formats(r[0])+' - '+formats(r[1]) ;
+    });
 })();
